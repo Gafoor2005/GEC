6f5305eafe07d6f223ff164e2d3b78642a0787b6
Gafoor <gafoorma686@gmail.com>
2023-07-16 18:42:52 +0530

    home completed & working on schedule

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 const navs = document.querySelectorAll("nav a");
-let navItems = [];
-navs.forEach((e)=>{
-    let i = e.href.split('#')[1];
-    console.log(i);
-    navItems.push(i)
-})
+let spans = document.querySelectorAll("nav a span");
+let main = document.querySelector("body > main");
+let secs = document.querySelectorAll('main > section');
 navs.forEach((e)=>{
 
     e.addEventListener("click",()=>{
         let i = e.href.split('#')[1];
         let span = e.querySelector('span');
         removeActive();
+        console.log("rem");
+        main.scrollBy(0,10);
         span.classList.add('navActive');
     })
 })
 function removeActive(){
-    let spans = document.querySelectorAll("nav a span");
     spans.forEach((e)=>{
         if(e.classList.contains("navActive")){
             e.classList.remove("navActive");
         }
     })
 }
+// console.log(main.scrollTop);
+main.addEventListener("scroll",()=>{
+    for(i=0;i<secs.length;i++){
+        let s = secs[i];
+        if(main.scrollTop <= (s.scrollHeight + s.offsetTop - main.offsetTop -5)){
+            navs.forEach((e)=>{
+                let span = e.querySelector('span');
+                if(s.id === e.href.split('#')[1]  &&  (!(span.classList.contains("navActive")))){
+                    console.log("set active");
+                    removeActive();
+                    span.classList.add('navActive');
+                }
+            })
+            break;
+        }
+    }
+})
 // -------------------nav animation end-----------------------
