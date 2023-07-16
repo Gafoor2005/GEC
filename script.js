e249965dc58ba2b3bf80e0732ef86996a624e1c4
Gafoor <gafoorma686@gmail.com>
2023-07-16 08:34:35 +0530

    nav working

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const navs = document.querySelectorAll("nav a");
-console.log(navs);
 let navItems = [];
 navs.forEach((e)=>{
     let i = e.href.split('#')[1];
     console.log(i);
     navItems.push(i)
 })
-console.log(navItems);
+navs.forEach((e)=>{
+
+    e.addEventListener("click",()=>{
+        let i = e.href.split('#')[1];
+        let span = e.querySelector('span');
+        removeActive();
+        span.classList.add('navActive');
+    })
+})
+function removeActive(){
+    let spans = document.querySelectorAll("nav a span");
+    spans.forEach((e)=>{
+        if(e.classList.contains("navActive")){
+            e.classList.remove("navActive");
+        }
+    })
+}
+// -------------------nav animation end-----------------------
